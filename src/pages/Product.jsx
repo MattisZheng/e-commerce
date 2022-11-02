d18b7a43b3ff524d6f8fc729b383c1bac23b8808
Mattis Zheng <mattiszheng@gmail.com>
2022-11-02 18:57:36 +0800

    update product, create recommendation

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,27 +1,39 @@
 import { useState, useEffect } from "react";
 
-const Product = () => {
+const Product = ({ item }) => {
   const [product, setProduct] = useState({
     id: 1,
     title: "title",
     price: 9999.99,
-    description:
-      "description",
+    description: "description",
     category: "category",
     image: "image link",
     rating: { rate: 0.0, count: 0 },
   });
 
   // fetch product from api
-  async function getProduct() {
-    const res = await fetch(`https://fakestoreapi.com/products/1`); // fetch from api
+  async function getProduct(item) {
+    const res = await fetch(`https://fakestoreapi.com/products/${item}`); // fetch from api
     const data = await res.json(); // parse json
     setProduct(data); // set data to state
   }
 
   useEffect(() => {
-    getProduct();
-    console.log(product);
+    getProduct(item);
+    console.log("item");
+  }, []);
+
+  const [recommendation, setRecommendation] = useState();
+
+  async function getRecommendation(category) {
+    const res = await fetch(`https://fakestoreapi.com/products/category/${category}`); // fetch from api
+    const data = await res.json(); // parse json
+    setRecommendation(data); // set data to state
+  }
+
+  useEffect(() => {
+    getRecommendation(product.category);
+    console.log("recommendation");
   }, [product]);
 
   return (
@@ -34,7 +46,21 @@ const Product = () => {
       add to cart
       <p>{product.description}</p>
       <p>{product.rating.rate}</p>
-      recommendation
+      <p>{product.rating.count}</p>
+      <div >
+        <div>
+          {recommendation &&
+            recommendation.map((item) => (
+              <div key={item.id}>
+                <img className="w-64" src={item.image} alt={item.title} />
+                <p>{item.title}</p>
+                <p>{item.price}</p>
+                <p>{item.rating.rate}</p>
+                <p>{item.rating.count}</p>
+              </div>
+            ))}
+        </div>
+      </div>
     </div>
   );
 };
