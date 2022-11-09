9a9a52425907e2842ef3222b20071c80e34f9bc2
Mattis Zheng <mattiszheng@gmail.com>
2022-11-09 19:16:00 +0800

    add login modal

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,19 +1,35 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Modal from "react-modal";
 
 const customStyles = {
+  overlay: {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: "rgba(255, 255, 255, 0.75)",
+    zIndex: 10,
+  },
   content: {
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)",
+    position: "absolute",
+    top: "40px",
+    left: "40px",
+    right: "40px",
+    bottom: "40px",
+    border: "1px solid #ccc",
+    background: "#fff",
+    overflow: "auto",
+    WebkitOverflowScrolling: "touch",
+    borderRadius: "4px",
+    outline: "none",
+    padding: "20px",
+    zIndex: 11,
   },
 };
 
-// Modal.setAppElement("#yourAppElement");
+Modal.setAppElement(document.getElementById("root"));
 
 const Auth = () => {
   // if token is incorrect, remove token from session storage and show login form
@@ -44,23 +60,30 @@ const Auth = () => {
   const LoginButton = (
     <div>
       <button onClick={openModal}>Open Modal</button>
-      <Modal isOpen={modalIsOpen} onAfterOpen={afterOpenModal} onRequestClose={closeModal} style={customStyles} contentLabel="Example Modal">
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+        preventScroll={true}
+      >
         <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
         <button onClick={closeModal}>close</button>
         <div>I am a modal</div>
-        <form>
-          <input />
-          <button>tab navigation</button>
-          <button>stays</button>
-          <button>inside</button>
-          <button>the modal</button>
+        <form action="">
+          <label htmlFor="">Username</label>
+          <input type="text" />
+          <label htmlFor="">Password</label>
+          <input type="text" />
+          <button>Submit</button>
         </form>
       </Modal>
     </div>
   );
   // if token is empty, show login form
 
-  return <div>{token === "..." ? LoginButton : LogoutButton}</div>;
+  return <div>{token === ".." ? LoginButton : LogoutButton}</div>;
 };
 
 export default Auth;
