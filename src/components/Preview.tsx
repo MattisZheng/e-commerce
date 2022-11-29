177e4cdfe1639e27331d0087efec453eb190d5b9
Mattis Zheng <mattiszheng@gmail.com>
2022-11-29 19:27:19 +0800

    update links

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 const Preview = ({ id, title, price, image, rating }: { id: string; title: string; price: number; image: string; rating: any }) => {
   return (
     <div className="h-auto border-black order-solid border-2 m-10 p-2 bg-slate-500 ">
-      <Link to={`./item/${id}`}>Link</Link>
-      <Link to={`./item/${id}`} target="_blank">
+      <Link to={`/item/${id}`}>Link</Link>
+      <Link to={`/item/${id}`} target="_blank">
         <img src={image} alt={title} className="h-48" />
         <p>{title}</p>
         <p className="h-auto">
--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -9,18 +9,12 @@ const SearchBox = () => {
     setKeyword(e.target.value);
   }
 
-  // go to url with keyword
-  function handleClick(e: any) {
-    console.log(e, keyword);
-    location.href = `/search/${keyword}`;
-  }
-
   return (
     <div>
       <div className="flex">
         <input type="text" id="keyword" placeholder="search" autoComplete="off" onChange={handleChange} className="rounded-full pl-4" />
         <div className="border-solid border-2 m-2">
-          <Link to={`./search/${keyword}`}>Link</Link>
+          <Link to={`/search/${keyword}`}>Link</Link>
         </div>
       </div>
       <Recommendation />
