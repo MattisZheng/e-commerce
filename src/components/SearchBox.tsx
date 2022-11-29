a8f1470d1673b467f94aebcf602f1aa2901d872a
Mattis Zheng <mattiszheng@gmail.com>
2022-11-29 19:01:30 +0800

    update searchbox

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import Recommendation from './Recommendation';
 
 const SearchBox = () => {
@@ -9,17 +10,21 @@ const SearchBox = () => {
   }
 
   // go to url with keyword
-  function handleSubmit(e: any) {
-    e.preventDefault();
+  function handleClick(e: any) {
+    console.log(e, keyword);
     location.href = `/search/${keyword}`;
   }
 
   return (
-    <form onSubmit={handleSubmit} className="border-solid border-2 m-2">
-      <input type="text" id="keyword" placeholder="search" autoComplete="off" onChange={handleChange} className="rounded-full pl-4" />
-      <button className="border-solid border-2 m-2">Search</button>
+    <div>
+      <div className="flex">
+        <input type="text" id="keyword" placeholder="search" autoComplete="off" onChange={handleChange} className="rounded-full pl-4" />
+        <div className="border-solid border-2 m-2">
+          <Link to={`/search/${keyword}`}>Link</Link>
+        </div>
+      </div>
       <Recommendation />
-    </form>
+    </div>
   );
 };
 
